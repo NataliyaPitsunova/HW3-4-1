8397d770b35ca4e1264bee9bae1cf25115a33b22
NataliyaPitsunova <nataillar1988@icloud.com>
2022-04-01 18:05:03 +0300

    Initial commit

---
+++ src/main/java/WaitNotify.java
@@ -0,0 +1,10 @@
+public class WaitNotify {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) throws InterruptedException {
+        WaitNotify waitNotifyObj = new WaitNotify();
+  
+    }
+}
+
